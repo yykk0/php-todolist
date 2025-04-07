2fbcd19833c835fc02c21afc737d1d110332ecf2
yykk0 <yuuuki96g@gmail.com>
2025-04-07 22:11:12 +0900

    データを用意#1

--- index.php
+++ index.php
@@ -12,11 +12,31 @@ require_once './database.php';
 
 <body>
     <?php
-    $db = getDB();
-    $stmt = $db->prepare('SELECT * FROM todos');
-    $stmt->execute();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        print_r($row);
+    try {
+        $db = getDB();
+        $stmt = $db->prepare('SELECT * FROM todos WHERE status != 3 && is_deleted = false && completed_at IS NULL ORDER BY priority DESC');
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+            <ul>
+                <p>タイトル：<?= $row['title'] ?></p>
+                <ul>
+                    <li>説明：<?= $row['description'] ?></li>
+                    <li>ステータス：<?= $row['status'] ?></li>
+                    <li>優先度：<?= $row['priority'] ?></li>
+                    <li>期限：<?= $row['due_date'] ?></li>
+                    <li>作成日：<?= $row['created_at'] ?></li>
+                    <li>更新日：<?= $row['updated_at'] ?></li>
+                    <li>削除フラグ：<?= $row['is_deleted'] ?></li>
+                    <li>カテゴリー：<?= $row['category'] ?></li>
+                    <li>タグ：<?= $row['tags'] ?></li>
+                    <li>完了日：<?= $row['completed_at'] ?></li>
+                </ul>
+            </ul>
+    <?php
+        }
+    } catch (PDOException $e) {
+        die("エラーメッセージ：{$e->getMessage()}");
     }
     ?>
 </body>
